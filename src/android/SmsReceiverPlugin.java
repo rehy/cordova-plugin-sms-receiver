61cdee21f686acb4f7ac30013af23bc7ad595b6c
Ratson <github@ratson.name>
2016-09-16 17:04:12 +0800

    Add requestPermission()

--- src/android/SmsReceiverPlugin.java
+++ src/android/SmsReceiverPlugin.java
@@ -1,8 +1,11 @@
 package name.ratson.cordova.sms_receiver;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -11,13 +14,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class SmsReceiverPlugin extends CordovaPlugin {
-    public final String ACTION_HAS_SMS_POSSIBILITY = "HasSMSPossibility";
-    public final String ACTION_RECEIVE_SMS = "StartReception";
-    public final String ACTION_STOP_RECEIVE_SMS = "StopReception";
-
+    private static final String REQUEST_READ_PERMISSION = "requestReadPermission";
+    public final String ACTION_HAS_SMS_POSSIBILITY = "hasSMSPossibility";
+    public final String ACTION_RECEIVE_SMS = "startReception";
+    public final String ACTION_STOP_RECEIVE_SMS = "stopReception";
     private CallbackContext callback_receive;
     private SmsReceiver smsReceiver = null;
     private boolean isReceiving = false;
+    private int requestCode = 20160916;
 
     public SmsReceiverPlugin() {
         super();
@@ -27,69 +31,94 @@ public class SmsReceiverPlugin extends CordovaPlugin {
     public boolean execute(String action, JSONArray arg1,
                            final CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals(ACTION_HAS_SMS_POSSIBILITY)) {
-
-            Activity ctx = this.cordova.getActivity();
-            if (ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
-            } else {
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, false));
-            }
+        if (ACTION_HAS_SMS_POSSIBILITY.equals(action)) {
+            hasSmsPossibility(callbackContext);
             return true;
-        } else if (action.equals(ACTION_RECEIVE_SMS)) {
-
-            // if already receiving (this case can happen if the startReception is called
-            // several times
-            if (this.isReceiving) {
-                // close the already opened callback ...
-                PluginResult pluginResult = new PluginResult(
-                        PluginResult.Status.NO_RESULT);
-                pluginResult.setKeepCallback(false);
-                this.callback_receive.sendPluginResult(pluginResult);
-
-                // ... before registering a new one to the sms receiver
-            }
-            this.isReceiving = true;
-
-            if (this.smsReceiver == null) {
-                this.smsReceiver = new SmsReceiver();
-                IntentFilter fp = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
-                fp.setPriority(1000);
-                // fp.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
-                this.cordova.getActivity().registerReceiver(this.smsReceiver, fp);
-            }
-
-            this.smsReceiver.startReceiving(callbackContext);
-
-            PluginResult pluginResult = new PluginResult(
-                    PluginResult.Status.NO_RESULT);
-            pluginResult.setKeepCallback(true);
-            callbackContext.sendPluginResult(pluginResult);
-            this.callback_receive = callbackContext;
-
+        } else if (ACTION_RECEIVE_SMS.equals(ation)) {
+            receiveSms(callbackContext);
             return true;
-        } else if (action.equals(ACTION_STOP_RECEIVE_SMS)) {
+        } else if (ACTION_STOP_RECEIVE_SMS.equals(action)) {
+            stopReceiveSms(callbackContext);
+            return true;
+        } else if (REQUEST_READ_PERMISSION.equals(action)) {
+            requestPermission(Manifest.permission.RECEIVE_SMS);
+            return true;
+        }
 
-            if (this.smsReceiver != null) {
-                smsReceiver.stopReceiving();
-            }
+        return false;
+    }
 
-            this.isReceiving = false;
+    private void stopReceiveSms(CallbackContext callbackContext) {
+        if (this.smsReceiver != null) {
+            smsReceiver.stopReceiving();
+        }
 
-            // 1. Stop the receiving context
+        this.isReceiving = false;
+
+        // 1. Stop the receiving context
+        PluginResult pluginResult = new PluginResult(
+                PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(false);
+        this.callback_receive.sendPluginResult(pluginResult);
+
+        // 2. Send result for the current context
+        pluginResult = new PluginResult(
+                PluginResult.Status.OK);
+        callbackContext.sendPluginResult(pluginResult);
+    }
+
+    private void receiveSms(CallbackContext callbackContext) {
+        // if already receiving (this case can happen if the startReception is called
+        // several times
+        if (this.isReceiving) {
+            // close the already opened callback ...
             PluginResult pluginResult = new PluginResult(
                     PluginResult.Status.NO_RESULT);
             pluginResult.setKeepCallback(false);
             this.callback_receive.sendPluginResult(pluginResult);
 
-            // 2. Send result for the current context
-            pluginResult = new PluginResult(
-                    PluginResult.Status.OK);
-            callbackContext.sendPluginResult(pluginResult);
+            // ... before registering a new one to the sms receiver
+        }
+        this.isReceiving = true;
 
-            return true;
+        if (this.smsReceiver == null) {
+            this.smsReceiver = new SmsReceiver();
+            IntentFilter fp = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+            fp.setPriority(1000);
+            // fp.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+            this.cordova.getActivity().registerReceiver(this.smsReceiver, fp);
         }
 
-        return false;
+        this.smsReceiver.startReceiving(callbackContext);
+
+        PluginResult pluginResult = new PluginResult(
+                PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        callbackContext.sendPluginResult(pluginResult);
+        this.callback_receive = callbackContext;
+    }
+
+    private void hasSmsPossibility(CallbackContext callbackContext) {
+        Activity ctx = this.cordova.getActivity();
+        if (ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY)) {
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, true));
+        } else {
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, false));
+        }
+    }
+
+    private boolean hasPermissionGranted(String type) {
+        if (Build.VERSION.SDK_INT < 23) {
+            return true;
+        }
+        return (PackageManager.PERMISSION_GRANTED ==
+                ContextCompat.checkSelfPermission(this.cordova.getActivity(), type));
+    }
+
+    private void requestPermission(String type) {
+        if (!hasPermissionGranted(type)) {
+            ActivityCompat.requestPermissions(this.cordova.getActivity(), new String[]{type}, requestCode);
+        }
+        this.callback.success();
     }
 }
